eb96c8290bb271e2102b1818192525444da548ad
Adnan <Adnan@DESKTOP-UGPNVH0>
2023-01-14 12:39:43 +0600

    Result page updated

---
+++ src/components/pages/Result.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Summary from "../Summary.js";
+import Analysis from "../Analysis.js";
+
+export default function Result() {
+  return (
+    <>
+      <Summary />
+      <Analysis />
+    </>
+  );
+}
